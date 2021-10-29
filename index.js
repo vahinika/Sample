5372f444d1b921653a096cae10c0c20e8339c225
vahinika <93057856+vahinika@users.noreply.github.com>
2021-10-29 19:08:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let app = express();
 let ejs = require('ejs');
-const haikus = require('./haikus.json');
+const haikus = require('');
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
+  res.render('index', {});
 });
 
 app.listen(port);
